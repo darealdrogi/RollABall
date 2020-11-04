50f0bb800bfaee28f743f8da198affdf339c2f33
darealdrogi <73650314+darealdrogi@users.noreply.github.com>
2020-11-04 10:51:13 +0100

    lesson2

---
+++ Assets/Script/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    [SerializeField] private int m_treshold = 10;
+    [SerializeField] private string m_message = "Hello World";
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogHelloWorld(m_message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void LogHelloWorld(string message)
+    {
+        if (m_treshold > 10)
+        Debug.Log(message);
+    }
+
+}
